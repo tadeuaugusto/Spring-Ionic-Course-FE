c84a9601d4d95608429229f5d32d587ae89a17eb
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2018-12-05 16:04:49 -0200

    Heroku endpoint and Extract email from token and saving to localStorage

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,6 @@
 export const API_CONFIG = {
-    baseUrl: 'http://localhost:8080',
+    // baseUrl: 'http://localhost:8080',
+    // baseUrl: 'http://spring-ionic-be.herokuapp.com',
+    baseUrl: 'https://spring-boot-ionic-1.herokuapp.com',
     bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic'
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,12 @@ import { Injectable } from '@angular/core';
 import { CredentialsDTO } from '../models/credentials.dto';
 import { HttpClient } from '@angular/common/http';
 import { API_CONFIG } from '../config/api.config';
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
+
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(public http: HttpClient, public storage: StorageService) {
 
@@ -26,7 +29,8 @@ export class AuthService {
     successfulLogin(authorizationValue : string) {
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
     }
